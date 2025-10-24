c7554be3640119b2c7db8a12fa42987323981216
David dos Santos Machado <david.s.machado99@gmail.com>
2025-10-24 00:18:27 -0300

    feature(dominio): definicao de mapas para base de dados

---
+++ backend/src/SAEA.Infrastructure/Database/Mappings/OperatingHoursMap.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SAEA.Domain.Models;
+
+namespace SAEA.Infrastructure.Database.Mappings
+{
+    public sealed class OperatingHoursMap : IEntityTypeConfiguration<OperatingHours>
+    {
+        public void Configure(EntityTypeBuilder<OperatingHours> builder)
+        {
+            builder.ToTable("TB_OPERATING_HOURS");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                   .HasColumnName("ST_OPERATING_HOURS_ID");
+
+            builder.Property(x => x.ResourceType)
+                   .HasColumnName("NB_RESOURCE_TYPE")
+                   .IsRequired()
+                   .HasConversion<int>();
+
+            builder.Property(x => x.ResourceId)
+                   .HasColumnName("ST_RESOURCE_ID")
+                   .IsRequired();
+
+            builder.Property(x => x.DayOfWeek)
+                   .HasColumnName("NB_DAY_OF_WEEK")
+                   .IsRequired()
+                   .HasConversion<int>();
+
+            builder.Property(x => x.StartTime)
+                   .HasColumnName("DT_START_TIME")
+                   .IsRequired()
+                   .HasConversion(
+                        time => time.ToTimeSpan(),
+                        timeSpan => TimeOnly.FromTimeSpan(timeSpan)
+                    );
+
+            builder.Property(x => x.EndTime)
+                   .HasColumnName("DT_END_TIME")
+                   .IsRequired()
+                   .HasConversion(
+                        time => time.ToTimeSpan(),
+                        timeSpan => TimeOnly.FromTimeSpan(timeSpan)
+                   );
+
+            builder.HasIndex(x => new { x.ResourceType, x.ResourceId })
+                   .HasDatabaseName("IX_OPERATING_HOURS_RESOURCE");
+
+            builder.HasIndex(x => x.DayOfWeek)
+                   .HasDatabaseName("IX_OPERATING_HOURS_DAY");
+        }
+    }
+}
---
+++ backend/src/SAEA.Infrastructure/Database/Mappings/RoomMap.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SAEA.Domain.Models;
+
+namespace SAEA.Infrastructure.Database.Mappings
+{
+    public sealed class RoomMap : IEntityTypeConfiguration<Room>
+    {
+        public void Configure(EntityTypeBuilder<Room> builder)
+        {
+            builder.ToTable("TB_ROOMS");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                   .HasColumnName("ST_ROOM_ID");
+
+            builder.Property(x => x.PavilionId)
+                   .HasColumnName("ST_PAVILION_ID")
+                   .IsRequired();
+
+            builder.Property(x => x.Name)
+                   .HasColumnName("ST_NAME")
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.Property(x => x.Code)
+                   .HasColumnName("ST_CODE")
+                   .IsRequired()
+                   .HasMaxLength(30);
+
+            builder.Property(x => x.Capacity)
+                   .HasColumnName("NB_CAPACITY")
+                   .IsRequired();
+            
+            builder.HasOne(x => x.Pavilion)
+                   .WithMany(x => x.Rooms)
+                   .HasForeignKey(x => x.PavilionId);
+
+            builder.HasMany(x => x.Desks)
+                   .WithOne(x => x.Room)
+                   .HasForeignKey(x => x.RoomId);
+
+            builder.HasIndex(x => x.Code)
+                   .HasDatabaseName("UN_ROOM_CODE")
+                   .IsUnique();
+        }
+    }
+}
