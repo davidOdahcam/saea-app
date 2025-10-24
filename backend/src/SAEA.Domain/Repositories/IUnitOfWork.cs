00b08e84b182612d4c3903ea07c081097d8432ac
David dos Santos Machado <david.s.machado99@gmail.com>
2025-10-24 17:16:38 -0300

    feature(pavilhao): repositorio base

---
+++ backend/src/SAEA.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace SAEA.Domain.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IPavilionRepository Pavilions { get; }
+
+        Task<int> CommitAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ backend/src/SAEA.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SAEA.Domain.Repositories;
+using SAEA.Infrastructure.Database.Context;
+using System.Linq.Expressions;
+
+namespace SAEA.Infrastructure.Repositories
+{
+    public class Repository<T>(SAEAContext context) : IRepository<T> where T : class
+    {
+        protected readonly SAEAContext _context = context;
+        protected readonly DbSet<T> _dbSet = context.Set<T>();
+
+        public virtual async Task<T?> GetByIdAsync(Guid id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public virtual async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public virtual void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public virtual void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public virtual async Task<bool> ExistsAsync(Guid id)
+        {
+            var entity = await GetByIdAsync(id);
+            return entity != null;
+        }
+
+        public virtual async Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null)
+        {
+            return predicate == null
+                ? await _dbSet.CountAsync()
+                : await _dbSet.CountAsync(predicate);
+        }
+    }
+}
